c28785b0e1fa0f84532c4bec3a420458caec156c
snowman6-git <chldlfgks55@gmail.com>
2026-05-18 22:38:11 +0900

    뷰포트를 옮기다.

--- frontend_svelte/src/routes/+page.svelte
+++ frontend_svelte/src/routes/+page.svelte
@@ -153,15 +153,7 @@
 	}
 </script>
 
-<!-- 줌(아줌마아님)밴 -->
-<meta
-	name="viewport"
-	content="width=device-width, 
-	initial-scale=1.0, 
-	maximum-scale=1.0, 
-	user-scalable=no,
-	"
-/>
+
 
 
 <!-- 어디든엔터라면채팅을넣어 -->
